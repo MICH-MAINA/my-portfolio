b54576243fc9c0c2472fd0f708e312c999b0dfa2
Michelle wangui maina <66360345+MICH-MAINA@users.noreply.github.com>
2023-06-26 12:31:52 +0300

    removed contact form

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ $(document).ready(function(){
        $('.menu-btn i').toggleClass("active");
     });
 });
+
+function readMore(){
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readMore");
+
+    if(dots.style.display === "none"){
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read More";
+        moreText.style.display = "none";
+    }else{
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+} 
